ad391b1ea9ff7f7a3d2f92549edfa5404c451245
Pete Vilter <7341+vilterp@users.noreply.github.com>
2021-09-30 16:28:46 -0500

    parsing edges kinda works

--- src/HeapSnapshotParser.jl
+++ src/HeapSnapshotParser.jl
@@ -5,12 +5,14 @@ using JSON3
 Base.@kwdef struct Node
     kind::Symbol
     type::String
+    num_edges::Int
 
     # out_edges::Array{Edge}
 end
 
 Base.@kwdef struct Edge
     kind::Symbol
+    name::String
 
     from::Node
     to::Node
@@ -25,7 +27,7 @@ NODE_FIElDS = ["type", "name", "id", "self_size", "edge_count", "trace_node_id",
 NUM_NODE_FIELDS = length(NODE_FIElDS)
 
 EDGE_FIELDS = ["type", "name_or_index", "to_node"]
-NUM_EDGE_FIELDS = length(NODE_FIElDS)
+NUM_EDGE_FIELDS = length(EDGE_FIELDS)
 
 function parse_snapshot(input::IOStream)::HeapSnapshot
     parsed = JSON3.read(input)
@@ -39,33 +41,53 @@ function parse_snapshot(input::IOStream)::HeapSnapshot
     for node_idx = 0:(num_nodes-1)
         kind_key = nodes[node_idx*NUM_NODE_FIELDS + 1]
         name_key = nodes[node_idx*NUM_NODE_FIELDS + 2]
+        num_edges = nodes[node_idx*NUM_NODE_FIELDS + 5]
 
         node = Node(
             kind=Symbol(node_kind_enum[kind_key + 1]),
             type=parsed.strings[name_key + 1],
+            num_edges=num_edges,
         )
 
         push!(snapshot.nodes, node)
     end
 
-    # edges = parsed.edges
-    # num_edges = convert(Int, length(edges)/NUM)
-    # for edge_idx = 0:(num_edges-1)
-    #     kind_key = edges[edge_idx*NUM_EDGE_FIELDS + 1]
-    #     name_key = edges[edge_idx*NUM_EDGE_FIELDS + 2]
-    #     to_key = edges[edge_idx*NUM_EDGE_FIELDS + 3]
+    edges = parsed.edges
+    edge_idx = 0
+    for from_node in snapshot.nodes
+        for edge_num = 1:(from_node.num_edges)
+            kind_key = edges[edge_idx*NUM_EDGE_FIELDS + 1]
+            name_key = edges[edge_idx*NUM_EDGE_FIELDS + 2]
+            to_key = edges[edge_idx*NUM_EDGE_FIELDS + 3]
 
-    #     edge = Edge(
-    #         kind=Symbol(edge_kind_enum[kind_key]),
-    #         name=XXXX,
-    #         from=XXX,
-    #         to=snapshot.nodes[XXXX]
-    #     )
+            to_node_idx = convert(Int, to_key/NUM_NODE_FIELDS) + 1
 
-    #     push!(snapshot.edges, edge)
-    # end
+            kind = Symbol(edge_kind_enum[kind_key + 1])
+
+            name = if kind == :internal
+                "<internal>"
+            elseif kind == :element
+                "<element>"
+            else
+                parsed.strings[name_key+1]
+            end
+
+            edge = Edge(
+                kind=kind,
+                name=name,
+                from=from_node,
+                to=snapshot.nodes[to_node_idx],
+            )
+
+            push!(snapshot.edges, edge)
+            edge_idx += 1
+        end
+    end
 
     return snapshot
 end
+
+# TODO: struct RawEdge
+# TODO: struct RawNode
 
 end # module
