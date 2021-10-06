7eef52be47530527a67cf08f73c7a0c0bdd0d48e
Pete Vilter <7341+vilterp@users.noreply.github.com>
2021-10-06 01:14:45 -0400

    draw graph
    make sure not to actually run this in CI cuz it'll take like an hour

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,10 @@ using HeapSnapshotParser
 # TODO: make this into an actual unit test
 # at the moment just testing that it doesn't explode
 
-open("../testdata/empty-repl.heapsnapshot") do f
+heap = open("../testdata/empty-repl.heapsnapshot") do f
     HeapSnapshotParser.parse_snapshot(f)
 end
+
+graph = HeapSnapshotParser.to_lightgraph(heap)
+plot = gplot(graph)
+draw(PNG("heap.png"), plot)
