bc27ffeb2537551258252d8de2284d5bdb8eb281
Pete Vilter <pete.vilter@gmail.com>
2024-02-01 01:08:58 +0000

    try constructing tree

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,10 @@ using Test
 # at the moment just testing that it doesn't explode
 
 snapshot = HeapSnapshotParser.parse_snapshot("../testdata/empty-repl.heapsnapshot")
+# snapshot = HeapSnapshotParser.parse_snapshot("Snapshot.heapsnapshot")
 @test length(snapshot.nodes) > 0
 @test length(snapshot.edges) > 0
 
 flame_graph = HeapSnapshotParser.get_flame_graph(snapshot)
 @test length(flame_graph.children) > 0
+@test flame_graph.parent === nothing
