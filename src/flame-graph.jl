9994bf0a79ce3d7e4c2482d498cc360e71d6efd4
Pete Vilter <pete.vilter@gmail.com>
2024-02-01 01:32:03 +0000

    passes tests now??
    Former-commit-id: 1fd4960949f6058e2617d005c588f6e85b88e3df

--- src/flame-graph.jl
+++ src/flame-graph.jl
@@ -14,31 +14,34 @@ function get_flame_graph(snapshot::HeapSnapshot)
     graph, seq_to_node = as_lightgraph(snapshot)
     undir = LightGraphs.SimpleGraph(graph)
     mst = LightGraphs.prim_mst(undir)
-    @show(mst)
     # convert to a flame graph
     nodes = Dict{Int, FlameNode}()
-    root = nothing
+    
+    nodes_with_no_parent = Set{Int}()
     
     for edge in mst
         if !haskey(nodes, edge.dst)
             nodes[edge.dst] = FlameNode(seq_to_node[edge.dst].type)
         end
+        push!(nodes_with_no_parent, edge.dst)
         if !haskey(nodes, edge.src)
             nodes[edge.src] = FlameNode(seq_to_node[edge.src].type)
         end
+        push!(nodes_with_no_parent, edge.src)
     end
+
     # set parents and children
     for edge in mst
         parent = nodes[edge.src]
         child = nodes[edge.dst]
+        delete!(nodes_with_no_parent, edge.dst)
         child.parent = parent
         push!(parent.children, child)
         # parent.self_value += snapshot.edges[(edge.src, edge.dst)].self_size
-        if root === nothing
-            println("setting parent")
-            root = parent
-        end
     end
     
-    return root
+    @assert length(nodes_with_no_parent) == 1
+    
+    root_id = collect(nodes_with_no_parent)[1]
+    return nodes[root_id]
 end
