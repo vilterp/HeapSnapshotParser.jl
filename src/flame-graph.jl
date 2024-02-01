8cfe1cfce44f2a4f5eb3dbc8e98dec3179e7e117
Pete Vilter <pete.vilter@gmail.com>
2024-02-01 01:08:58 +0000

    try constructing tree
    Former-commit-id: bc27ffeb2537551258252d8de2284d5bdb8eb281

--- src/flame-graph.jl
+++ src/flame-graph.jl
@@ -1,11 +1,13 @@
 mutable struct FlameNode
     name::String
     self_value::Float64
+    total_value::Float64
+    parent::Union{FlameNode,Nothing}
     children::Vector{FlameNode}
 end
 
 function FlameNode(name::String)
-    return FlameNode(name, 0.0, [])
+    return FlameNode(name, 0.0, 0.0, nothing, [])
 end
 
 function get_flame_graph(snapshot::HeapSnapshot)
@@ -14,9 +16,8 @@ function get_flame_graph(snapshot::HeapSnapshot)
     mst = LightGraphs.prim_mst(undir)
     # convert to a flame graph
     nodes = Dict{Int, FlameNode}()
-    root = FlameNode("root")
-    nodes[-1] = root
-    # TODO: how do we find the root in the snapshot?
+    root = nothing
+    
     for edge in mst
         if !haskey(nodes, edge.src)
             nodes[edge.src] = FlameNode(seq_to_node[edge.src].type)
@@ -25,11 +26,16 @@ function get_flame_graph(snapshot::HeapSnapshot)
             nodes[edge.dst] = FlameNode(seq_to_node[edge.dst].type)
         end
     end
+    # set parents and children
     for edge in mst
         parent = nodes[edge.src]
         child = nodes[edge.dst]
-        # parent.self_value += snapshot.edges[(edge.src, edge.dst)].self_size
+        child.parent = parent
         push!(parent.children, child)
+        # parent.self_value += snapshot.edges[(edge.src, edge.dst)].self_size
+        if root === nothing
+            root = parent
+        end
     end
     
     return root
