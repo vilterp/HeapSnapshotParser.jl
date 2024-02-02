bdf0158894c94a834c1b956ae22a54fda0a192f1
Pete Vilter <pete.vilter@gmail.com>
2024-02-02 22:42:03 +0000

    parse more stuff
    Former-commit-id: 9a4670f1baaa12985cf8a50f2545d8bb206f896e

--- src/HeapSnapshotParser.jl
+++ src/HeapSnapshotParser.jl
@@ -44,7 +44,19 @@ function HeapSnapshot()
     )
 end
 
+struct RawSnapshotMeta
+    node_fields::Vector{String}
+    node_types::Vector{Union{String, Vector{String}}}
+    edge_fields::Vector{String}
+    edge_types::Vector{Union{String, Vector{String}}}
+end
+
+struct RawSnapshotSnapshot
+    meta::RawSnapshotMeta
+end
+
 struct RawSnapshot
+    snapshot::RawSnapshotSnapshot
     nodes::Vector{Int}
     edges::Vector{Int}
     strings::Vector{String}
