866fae74067ee8152c2b69b4c590fb35fd757e26
Pete Vilter <pete.vilter@gmail.com>
2024-02-02 00:51:37 +0000

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,6 @@ using Test
 using JSON
 
 @testset "tiny" begin
-    # snapshot = HeapSnapshotParser.parse_snapshot("../testdata/empty-repl.heapsnapshot")
     snapshot = HeapSnapshotParser.parse_snapshot("../testdata/tiny.heapsnapshot")
     @test length(snapshot.nodes) > 0
     @test length(snapshot.edges) > 0
@@ -63,6 +62,98 @@ using JSON
         "5: current task": {
             "name": "Task",
             "num_children": 0,
+            "self_value": 0,
+            "total_value": 0,
+            "children": {}
+        }
+    }
+}
+""")
+    
+    @test JSON.json(dict, 4) == expected
+end
+
+@testset "big" begin
+    snapshot = HeapSnapshotParser.parse_snapshot("../Snapshot.heapsnapshot")
+    @test length(snapshot.nodes) > 0
+    @test length(snapshot.edges) > 0
+    
+    @info "getting flame graph"
+
+    flame_graph = HeapSnapshotParser.get_flame_graph(snapshot)
+    @test length(flame_graph.children) > 0
+    @test flame_graph.parent === nothing
+    # @test flame_graph.total_value > 0
+    
+    dict = HeapSnapshotParser.as_json(flame_graph; threshold=1)
+    @test length(dict["children"]) > 0
+    
+    expected = (
+"""{
+    "name": "",
+    "num_children": 9,
+    "self_value": 0,
+    "total_value": 0,
+    "children": {
+        "7: current task": {
+            "name": "Task",
+            "num_children": 7,
+            "self_value": 0,
+            "total_value": 0,
+            "children": {}
+        },
+        "4: root task": {
+            "name": "Task",
+            "num_children": 6,
+            "self_value": 0,
+            "total_value": 0,
+            "children": {}
+        },
+        "3: current task": {
+            "name": "Task",
+            "num_children": 8,
+            "self_value": 0,
+            "total_value": 0,
+            "children": {}
+        },
+        "8: main_module": {
+            "name": "Main",
+            "num_children": 32,
+            "self_value": 0,
+            "total_value": 0,
+            "children": {}
+        },
+        "6: root task": {
+            "name": "Task",
+            "num_children": 7,
+            "self_value": 0,
+            "total_value": 0,
+            "children": {}
+        },
+        "0: root task": {
+            "name": "Task",
+            "num_children": 8,
+            "self_value": 0,
+            "total_value": 0,
+            "children": {}
+        },
+        "2: root task": {
+            "name": "Task",
+            "num_children": 6,
+            "self_value": 0,
+            "total_value": 0,
+            "children": {}
+        },
+        "1: current task": {
+            "name": "Task",
+            "num_children": 8,
+            "self_value": 0,
+            "total_value": 0,
+            "children": {}
+        },
+        "5: current task": {
+            "name": "Task",
+            "num_children": 8,
             "self_value": 0,
             "total_value": 0,
             "children": {}
