be635f999c821cf495291b4d4bad6246da750ded
Pete Vilter <pete.vilter@gmail.com>
2024-02-05 10:53:18 -0500

    remove dead code

--- src/raw-snapshot.jl
+++ src/raw-snapshot.jl
@@ -1,24 +1,3 @@
-# ==== 'raw' version ====
-
-struct RawSnapshotMeta
-    node_fields::Vector{String}
-    node_types::Vector{Union{String, Vector{String}}}
-    edge_fields::Vector{String}
-    edge_types::Vector{Union{String, Vector{String}}}
-end
-
-struct RawSnapshotSnapshot
-    meta::RawSnapshotMeta
-end
-
-# JSON3 parses directly into this
-struct RawSnapshot
-    snapshot::RawSnapshotSnapshot
-    nodes::Vector{Int}
-    edges::Vector{Int}
-    strings::Vector{String}
-end
-
 # ==== 'parsed' version ====
 
 const NodeIdx = Int
