8eea180cec3e4b2581dbc3445017bad74fc21d45
Pete Vilter <pete.vilter@gmail.com>
2024-02-09 18:36:31 -0500

    delete some dead code

--- src/spanning-tree.jl
+++ src/spanning-tree.jl
@@ -146,33 +146,14 @@ function compute_sizes!(root::TreeNode)
     end
 end
 
-function as_json(snapshot::ParsedSnapshot, node::TreeNode; cur_depth=0, max_depth=10000)
-    children = get_relevant_children(node; cur_depth=cur_depth, max_depth=max_depth)
-    node_name = snapshot.strings[node.node.name]
-    return Dict(
-        "name" => if node.attr_name === nothing
-            node_name
-        else
-            "$(node.attr_name): $(node_name)"
-        end,
-        "self_value" => node.self_value,
-        "total_value" => node.total_value,
-        "num_children" => length(node.children),
-        "children" => [
-            as_json(snapshot, child; cur_depth=cur_depth+1, max_depth=max_depth)
-            for child in children
-        ]
-    )
-end
+const NORMAL_PRIORITY = 1
+const AVOID_PRIORITY = -1
 
-# return the top 10 nodes by total value
-# TODO: "rest" node
-function get_relevant_children(node::TreeNode; cur_depth=0, max_depth=10000, top_n=5)
-    if cur_depth > max_depth
-        return []
+function get_priority(avoid_set::Set{Int}, node::RawNode)
+    if node.name in avoid_set
+        return AVOID_PRIORITY
     end
-    sorted = sort(node.children, by=child -> child.total_value, rev=true)
-    return first(sorted, top_n)
+    return NORMAL_PRIORITY
 end
 
 # ====== stack
@@ -185,16 +166,6 @@ struct Stack
     function Stack()
         return new(Vector{TreeNode}(), Vector{Int}())
     end
-end
-
-const NORMAL_PRIORITY = 1
-const AVOID_PRIORITY = -1
-
-function get_priority(avoid_set::Set{Int}, node::RawNode)
-    if node.name in avoid_set
-        return AVOID_PRIORITY
-    end
-    return NORMAL_PRIORITY
 end
 
 function Base.push!(stack::Stack, node::TreeNode)
