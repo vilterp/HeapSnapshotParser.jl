7033417a9adfa160d41bb19fa37e145c05abb0c9
Pete Vilter <pete.vilter@gmail.com>
2024-02-11 16:12:56 -0500

    fix

--- src/sparse-matrix.jl
+++ src/sparse-matrix.jl
@@ -4,7 +4,8 @@ function make_sparse_matrix(snapshot::ParsedSnapshot)
     n = length(snapshot.nodes)
     matrix = spzeros(n, n)
     for (i, node) in enumerate(snapshot.nodes)
-        for edge in snapshot.edges[node.edge_indexes]
+        for edge_idx in node.edge_indexes
+            edge = snapshot.edges[edge_idx]
             matrix[i, edge.to] = edge.name
         end
     end
