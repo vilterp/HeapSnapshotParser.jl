63df1313a9c98dedcd7505e64da70a5a499e5894
Pete Vilter <pete.vilter@gmail.com>
2024-02-11 16:18:19 -0500

    try building it a different way

--- src/sparse-matrix.jl
+++ src/sparse-matrix.jl
@@ -1,13 +1,23 @@
 using SparseArrays
 
 function make_sparse_matrix(snapshot::ParsedSnapshot)
-    n = length(snapshot.nodes)
-    matrix = spzeros(n, n)
-    for (i, node) in enumerate(snapshot.nodes)
+    n = length(snapshot.edges)
+    
+    lefts = zeros(Int, n)
+    rights = zeros(Int, n)
+    values = zeros(Int, n)
+    
+    i = 0
+    for node in snapshot.nodes
         for edge_idx in node.edge_indexes
+            i += 1
+            
             edge = snapshot.edges[edge_idx]
-            matrix[i, edge.to] = edge.name
+            lefts[i] = node.id
+            rights[i] = edge.to
+            values[i] = edge.name
         end
     end
-    return matrix
+    
+    return sparse(lefts, rights, values)
 end
