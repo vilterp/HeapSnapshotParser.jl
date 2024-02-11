fa0f119159018fdc452f9299d827b58be87b06c0
Pete Vilter <pete.vilter@gmail.com>
2024-02-11 16:20:47 -0500

    build the matrix differently

--- src/sparse-matrix.jl
+++ src/sparse-matrix.jl
@@ -8,12 +8,13 @@ function make_sparse_matrix(snapshot::ParsedSnapshot)
     values = zeros(Int, n)
     
     i = 0
-    for node in snapshot.nodes
+    for node_idx in 1:length(snapshot.nodes)
+        node = snapshot.nodes[node_idx]
         for edge_idx in node.edge_indexes
             i += 1
             
             edge = snapshot.edges[edge_idx]
-            lefts[i] = node.id
+            lefts[i] = node_idx
             rights[i] = edge.to
             values[i] = edge.name
         end
